1558873581c5ec6950e564a51ef35e75096afa4f
Michael Lewis <factotuminok@gmail.com>
2025-01-09 16:38:04 -0600

    fix database ef update

--- LEGO_Inventory/entrypoint.sh
+++ LEGO_Inventory/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-run_cmd="dotnet run --server.urls http://*:8080"
+#run_cmd="dotnet run --server.urls http://*:8080"
 
 until dotnet ef database update; do
 >&2 echo "SQL Server is starting up"
-sleep 1
+sleep 5
 done
 
 >&2 echo "SQL Server is up - executing command"
