d314acf83c2b95e9928f068add00300c7eae856a
Thomas Lewis <thomas.a.lewis@proton.me>
2025-12-15 18:02:46 -0700

    Added User and UserIdentity files

---
+++ LEGO_Inventory/Models/User.cs
@@ -0,0 +1,14 @@
+namespace LEGO_Inventory;
+
+public class User
+{
+    public Guid UserId { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+    
+    public Guid Salt { get; set; }
+    
+    public int SaltIndex { get; set; }
+}
---
+++ LEGO_Inventory/Models/UserIdentity.cs
@@ -0,0 +1,8 @@
+namespace LEGO_Inventory;
+
+public class UserIdentity
+{
+    public Guid UserId { get; set; }
+    
+    public string Token { get; set; }
+}
